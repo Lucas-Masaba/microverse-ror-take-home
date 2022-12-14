2d0d65239bb4ac2507f31914d5cbf3f309f156d6
Lucas-Masaba <masabaluke@gmail.com>
2022-12-14 14:52:46 +0300

    Try to filter users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
-  USERS_PER_PAGE = 3
+  USERS_PER_PAGE = 5
   def index
     @page = params.fetch(:page, 0).to_i
-    @users = User.offset(@page * USERS_PER_PAGE).limit(USERS_PER_PAGE)
+    # @users = User.offset(@page * USERS_PER_PAGE).limit(USERS_PER_PAGE)
+  
+    @users = if params[:query].present?
+      User.where('status LIKE ?', "%#{params[:query]}%")
+    else    
+      User.offset(@page * USERS_PER_PAGE).limit(USERS_PER_PAGE)
+
+    end
   end
 
+  
   def show
     @user = User.find(params[:id])
   end
