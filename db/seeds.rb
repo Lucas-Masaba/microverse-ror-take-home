37bd59c28c4867b88235e49a9a231444da8074b1
Lucas-Masaba <masabaluke@gmail.com>
2022-12-14 03:25:47 +0300

    Add seed data from external api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'rest-client'
+
+url = 'https://microverse-api-app.herokuapp.com/users'
+response = RestClient.get url, {:Authorization => 'An32iK-eRu17'}
+
+users = JSON.parse(response)
+
+users.map { |user|
+  new_user = User.create(
+    api_id: user['id'],
+    first_name: user['first_name'],
+    last_name: user['last_name'],
+    status: user['status'],
+    created_at: user['created_at'],
+    email: user['email']
+  )
+}
+
