ef71df92a021a0aecf7a65c9bbe827b4b81c1163
Lucas-Masaba <masabaluke@gmail.com>
2022-12-14 03:24:46 +0300

    Migrate tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_12_13_233814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.integer "api_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "status"
+    t.date "created_at"
+    t.string "email"
+  end
 
 end
